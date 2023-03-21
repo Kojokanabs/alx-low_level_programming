1c823bf1f3283fc8469d1a73c23c94d00ccf6d32
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-21 06:08:22 -0700

    using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints "programming is like building a multilingual puzzle"
- * return:0 (Sucessful if zero is return)
+ * main - entry point
+ * return:0
  */
 int main(void)
 {
