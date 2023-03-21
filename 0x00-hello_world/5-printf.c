60c9b9bcfa365e102a494913f6c824f5aed96bb0
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-21 03:05:40 -0700

    Using printf fuction to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that print using printf fuction
+ * return:0 (sucessful)
+ */
+
+int main(void);
+{
+	printf("with a proper grammer,but the outcome is a piece of art,\n")
+	return (0);
+}
+
