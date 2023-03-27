e1d5f31bfe3b0e71202219847f2391fa0457fe08
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-27 15:08:09 -0700

    updated last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: printing of alphabets
+ *Return: 0 always
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putcher('\n');
+	return ('0');
+}
