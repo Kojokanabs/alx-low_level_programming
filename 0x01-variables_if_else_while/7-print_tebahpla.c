148bb92f341385d4689b7dcc132ef089f61a41fb
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-27 19:30:16 -0700

    printing lower case from z

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: reverse alphabets
+ *Return: 0 always
+*/
+int main(void)
+{
+	char n;
+
+	for (n = 'z' ; n >= 'a' ; n--)
+		putchar(n);
+	putchar('\n');
+	return ('0');
+}
