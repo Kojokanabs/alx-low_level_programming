9d19746a1ba4721423dfaf90f0db0485c1166e76
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-28 03:08:20 -0700

    update the printing of base 10

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,14 +12,14 @@ int main(void)
 
 	for (n = 0 ; n < 10 ; n++)
 	{
-		if (n == 9)
-			putchar(n + '0');
-		else
+
+		putchar(n + '0');
+		if (n != 9)
 		{
-			putchar(n + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
