aec19fd7129a4d5efbd717a4d2382ec712d2ad59
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-28 01:26:14 -0700

    printing number base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: printing base 16
+ * Return: 0 always
+*/
+int main(void)
+{
+	int n;
+	char j;
+
+	for (n = 0 ; n < 10 ; n++)
+		putchar(n + '0');
+	for (j = 'a'; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return ('0');
+}
