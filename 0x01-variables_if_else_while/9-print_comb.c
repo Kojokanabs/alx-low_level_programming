130f363d6830f3ad3b950f783c9345620f67b54a
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-28 02:25:57 -0700

    printing of base 10 saparated by comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: printing space followed by comma
+ *Return: 0 always
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		if (n == 9)
+			putchar(n + '0');
+		else
+			putchar(n + '0');
+			putchar(',');
+			putchar(' ');
+	}
+	return (0);
+}
