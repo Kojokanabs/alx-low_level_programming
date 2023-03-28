8098e04169b4f5b9df643186387e3d89a529753a
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-28 04:37:40 -0700

    printing with combination of two saparate digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: printing with many condition
+ * Return: 0 always
+*/
+int main(void)
+{
+	int n;
+	int j;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		for (j = 1 ; j < 10 ; j++)
+			if (n < j && n != j)
+			{
+				putchar(n + '0');
+				putchar(j + '0');
+				if (n + j != 17)
+				{
+					putchar(' ');
+					putchar(',');
+				}
+			}
+	}
+	putchar('\n');
+	return (0);
+
+}
