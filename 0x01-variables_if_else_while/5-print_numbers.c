f9a71f949b7887054144f801ba4e41ab50bf15e3
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-27 18:12:17 -0700

    all is well

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - entry point
+ *Description: 10 excluded
+ *Return: 0 always
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+
+}
