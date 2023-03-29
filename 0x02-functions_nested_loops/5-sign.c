e521aee64897b3542373fd93e37182604dbff8ca
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-29 15:16:16 -0700

    function that prints signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  *
  *@n: parameter to be checked
  *
- * Return: 1, 0 and -1
+ *Return: 1, 0 and -1
 */
 
 int print_sign(int n)
@@ -12,15 +12,23 @@ int print_sign(int n)
 	if (n > 1)
 	{
 		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		return (0);
 	}
 	else
-		_putchar(-');
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
 		return (-1);
+	}
 
 }
