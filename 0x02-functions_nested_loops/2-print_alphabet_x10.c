be4050c8c3b5ab9bac791667f8c937c2d62ca110
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-29 05:29:50 -0700

    printing of alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - printing of alphabets by 10
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char n;
+
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (n = 'a' ; n <= 'z' ; n++)
+			_putchar(n);
+
+	}
+}
