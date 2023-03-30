6dde722faeeed2bbbacb13a09ed68b65c085f615
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-30 02:47:24 -0700

    printing of absolute numbers

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,3 +1,16 @@
 #include "main.h"
 /**
+ *_abs - printing only positive side of a number
  *
+ *@n: number to be checked
+ *
+ *Return: @n
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		n = -(n)
+	else if (n >= 0)
+		n = n
+	return (n);
+}
