b6ca033c98abb84bb68d48e39b9549b7f2fe8d6d
Enoch K. Ali <enochkanabia@gmail.com>
2023-03-30 05:32:56 -0700

    printing of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - printing the sum of integers
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ *Return: a + b
+*/
+int add(int a, int b)
+{
+	int a;
+	int b;
+
+	return (a + b);
+}
